f23ebac4715b4ed1f582b7569b2404c0681392d9
Antony-1101 <109079263+Antony-1101@users.noreply.github.com>
2024-01-11 15:36:14 +0000

    add delete button
    add working delete button on every recipe card, on home page

--- js/view.js
+++ js/view.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const currentRecipeData = JSON.parse(localStorage.getItem('currentRecipe'));
 
     if (currentRecipeData) {
-        // Populate recipe details on the view.html page
         document.getElementById('recipeTitle').innerHTML = `<h2>${currentRecipeData.title}</h2>`;
         document.getElementById('prepTime').textContent = `Prep time: ${currentRecipeData.prepTime}`;
         document.getElementById('cookTime').textContent = `Cook time: ${currentRecipeData.cookTime}`;
@@ -18,11 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('difficulty').textContent = `Difficulty: ${currentRecipeData.difficulty}`;
         document.getElementById('isVegetarian').textContent = `Vegetarian: ${currentRecipeData.isVegetarian ? 'Yes' : 'No'}`;
         document.getElementById('isVegan').textContent = `Vegan: ${currentRecipeData.isVegan ? 'Yes' : 'No'}`;
-
-        // Populate image
         document.getElementById('recipeImage').innerHTML = `<img src="${currentRecipeData.image}">`;
 
-        // Populate ingredients
         const ingredientList = document.getElementById('ingredientList');
         ingredientList.innerHTML = '<h5>Ingredients</h5>';
         for (const category in currentRecipeData.ingredients) {
@@ -33,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function () {
             ingredientList.innerHTML += '</ul>';
         }
 
-        // Populate instructions
         const methodSection = document.getElementById('method');
         methodSection.innerHTML = '<h5>Method</h5>';
         currentRecipeData.instructions.forEach(instruction => {
